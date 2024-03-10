84511d9a1f0809f90e9dba2b62ef8d9b8f7e180c
Tomas Gonzalez <gonzalez.tomas@correo.unimet.edu.ve>
2024-03-10 14:43:32 -0400

    Testing github automation

--- src/components/navigation/Header.jsx
+++ src/components/navigation/Header.jsx
@@ -27,7 +27,7 @@ export default function Header() {
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
               <a className="nav-link" href="/dashboard">
-                Clubes
+                Club
               </a>
             </li>
             <li className="nav-item">
